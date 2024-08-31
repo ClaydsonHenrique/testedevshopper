e52add0f42066506d32e86f4da065f1a8afc095f
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-08-30 23:59:03 -0300

    criando alguns teste e corrigindo algun erros de digitacao

--- src/middleware/validateResquestPatch.ts
+++ src/middleware/validateResquestPatch.ts
@@ -10,7 +10,7 @@ const validatePatchRequest = async (
 
   if (!measure_uuid || !confirmed_value) {
     return res.status(400).json({
-      erro_code: "INVALID_DATA",
+      error_code: "INVALID_DATA",
       error_description:
         "O campo 'measure_uuid' e 'confirmed_value'são obrigatório.",
     });
@@ -18,7 +18,7 @@ const validatePatchRequest = async (
 
   if (typeof measure_uuid !== "string" || typeof confirmed_value !== "number") {
     return res.status(400).json({
-      erro_code: "INVALID_DATA",
+      error_code: "INVALID_DATA",
       error_description:
         "O parâmetro 'measure_uuid' deve ser uma string e 'confirmed_value' deve ser um número.",
     });
@@ -28,13 +28,13 @@ const validatePatchRequest = async (
 
   if (!measureRecord) {
     return res.status(404).json({
-      erro_code: "MEASURE_NOT_FOUND",
+      error_code: "MEASURE_NOT_FOUND",
       error_description: "Leitura do mês já realizada",
     });
   }
   if (measureRecord.value_confirmed) {
     return res.status(409).json({
-      erro_code: "CONFIRMATION_DUPLICATE",
+      error_code: "CONFIRMATION_DUPLICATE",
       error_description: "Leitura do mês já realizada",
     });
   }
